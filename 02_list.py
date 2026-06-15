99613d1a0bdc814fad08d6f8e7c026c7ff89924b
Mohammad Erza Faizi <mohammaderzafaizi@gmail.com>
2026-06-15 15:45:11 +0700

    belajar list gw bray

---
+++ 02_list.py
@@ -0,0 +1,5 @@
+jalur_pendakian_arjuno = ['sumberbrantas', 'tretes', 'purwosari', 'lawang']
+jalur_pendakian_arjuno.append('kaliandra')
+
+for jalur in jalur_pendakian_arjuno:
+    print(f"pendaki melewati jalur: {jalur}")
