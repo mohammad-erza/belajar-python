2ff73422c998e6fa551d0434322c1b2dda273510
Mohammad Erza Faizi <mohammaderzafaizi@gmail.com>
2026-06-18 11:29:41 +0700

    apa ya

--- 02_list.py
+++ 02_list.py
@@ -1,5 +1,3 @@
-jalur_pendakian_arjuno = ['sumberbrantas', 'tretes', 'purwosari', 'lawang']
-jalur_pendakian_arjuno.append('kaliandra')
-
-for jalur in jalur_pendakian_arjuno:
-    print(f"pendaki melewati jalur: {jalur}")
+nama_gunung = ["Semeru", "Rinjani", "Ciremai", "Slamet", "Sumbing"]
+urutan = sorted(nama_gunung, key=len)
+print(f"Urutan nama gunung berdasarkan panjang karakter: {urutan}")
