ccf439c260049eaec52f227e341d32598b7fcabf
Mohammad Erza Faizi <mohammaderzafaizi@gmail.com>
2026-06-22 21:37:32 +0700

    memperkuat fondasi fungsi di colab

--- 05_function.py
+++ 05_function.py
@@ -1,4 +1,12 @@
 def celcius_ke_fahrenheit(celcius):
   fahrenheit = (celcius * 1.8) + 32
   return fahrenheit
-celcius_ke_fahrenheit(2)
+celcius = int(input("Masukkan suhu dalam celcius: "))
+hasil1 = celcius_ke_fahrenheit(celcius)
+print(hasil1)
+
+def luas_segitiga(alas, tinggi):
+  luas = (alas * tinggi) / 2
+  return luas
+hasil = luas_segitiga(10, 5)
+print(hasil)
